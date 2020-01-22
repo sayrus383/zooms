e1fb88ee6f8b0ea547b8bc80b7f3f8a2af45f613
sayrus <sayrus383@mail.ru>
2020-01-22 13:19:25 +0600

    Add migrations

---
+++ database/migrations/2020_01_22_070137_create_companies_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+class CreateCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->decimal('lat', 10, 8);
+            $table->decimal('lng', 11, 8);
+            $table->timestamps();
+        });
+
+        DB::unprepared(file_get_contents(base_path('database/migrations/companies.sql')));
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+}
